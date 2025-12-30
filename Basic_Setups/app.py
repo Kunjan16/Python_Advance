8e6dbf6920eb31ab80f8a2c7c201d7f32e22b758
Kunjan Kumar Singh <kunjansingh4547@gmail.com>
2025-12-30 23:28:37 +0530

    add additional examples and explanations for variables, data types, and memory addresses in app.py

--- Basic_Setups/app.py
+++ Basic_Setups/app.py
@@ -1,2 +1,30 @@
 print("Hello World")
 print("*" * 10)
+student_count = 1000
+age = 20
+age = "Python"
+print(age)
+x=1
+print(id(x))  # prints the memory address of variable x
+
+x = x + 1
+print(id(x))  # prints the memory address of variable x after increment(Since integers are immutable, a new memory address is assigned)
+
+x = [1, 2, 3]
+print(id(x))  # prints the memory address of variable x
+
+x.append(4)
+print(id(x))  # prints the memory address of variable x after modification (Since lists are mutable, the same memory address is used)
+
+course = "Python Programming"
+print(len(course))  # prints the length of the string stored in variable course
+print(course[0])  # prints the first character of the string stored in variable course
+print(course[-1])  # prints the last character of the string stored in variable course
+print(course[0:5])  # prints the substring from index 0 to 5 of the string stored in variable course, characters at index 5 is not included
+print(course[:5])  # prints the substring from start to index 5 of the string stored in variable course
+print(course[0:])  # prints the substring from index 0 to end of the string stored in variable course
+print(course[:])  # prints the entire string stored in variable course
+
+print(id(course))  # prints the memory address of variable course
+print(id(course[0]))  # prints the memory address of the first character of the string stored in variable course
+# Strings are immutable, so modifying a character will create a new string
