6fd5d3e7358a63a0d8f16d5464c5b685624fe6b3
Kunjan Kumar Singh <kunjansingh4547@gmail.com>
2026-01-14 00:18:33 +0530

    feat: add list.py with examples of list operations and manipulations

---
+++ Data_Structures/list.py
@@ -0,0 +1,11 @@
+letters = ['a', 'b', 'c', 'd', 'e']
+matrix = [[0, 'a'], [1, 'b'], [2, 'c'], [3, 'd'], [4, 'e']]
+zeros = [0] * 5
+print(zeros)  # prints [0, 0, 0, 0, 0]
+combined = letters + zeros
+print(combined)  # prints ['a', 'b', 'c', 'd', 'e', 0, 0, 0, 0, 0]
+number = list(range(5))  # prints [0, 1, 2, 3, 4]
+print(number)
+chars = list("Hello World")  # prints ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
+print(chars)
+print(len(chars))  # prints 11
