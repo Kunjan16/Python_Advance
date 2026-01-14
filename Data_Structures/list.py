313fd0da5b289f7450e34c5a9e6008c4758d8903
Kunjan Kumar Singh <kunjansingh4547@gmail.com>
2026-01-14 23:56:51 +0530

    feat: add examples of unpacking and slicing in list.py

--- Data_Structures/list.py
+++ Data_Structures/list.py
@@ -16,3 +16,16 @@ print(letters[1:4])  # prints ['b', 'c', 'd']
 print(letters[::2])  # prints ['a', 'c', 'e']
 print(letters[::-1])  # prints ['e', 'd', 'c', 'b', 'a']
 print(letters[:])  # prints ['a', 'b', 'c', 'd', 'e']
+numbers = [1, 2, 3, 4, 5]
+first = numbers[0]
+second = numbers[1]
+third = numbers[2]
+fourth = numbers[3]
+fifth = numbers[4]
+
+first, second, third, fourth, fifth = numbers #what we have here is same as above but in a single line
+print(first, second, third, fourth, fifth)  # prints 1 2 3 4 5
+first, *middle, last = numbers
+print(first)   # prints 1   
+print(middle)  # prints [2, 3, 4]  
+print(last)    # prints 5
