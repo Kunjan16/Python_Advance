a9456b37814d433952dca9baeaa1dec290b013ab
Kunjan Kumar Singh <kunjansingh4547@gmail.com>
2026-01-17 00:47:13 +0530

    feat: add function to find second smallest and second largest elements in a sorted list

--- Data_Structures/listsort.py
+++ Data_Structures/listsort.py
@@ -27,3 +27,28 @@ for i in range(1, n):
         largest = arr[i]
 
 print(largest)            
+
+
+number = [1,2,4,7,7,5]
+numbers = sorted(number)
+
+n = len(numbers)
+def find_second_element(numbers):
+    n = len(numbers)
+    first = numbers[0]
+    for i in range(n):
+        if numbers[i] != first:
+            smallest = numbers[i]
+            break
+
+    last = numbers[-1]
+    for i in range(n-1, -1, -1):
+        if numbers[i] != last:
+            largest = numbers[i]
+            break
+
+    return smallest, largest
+
+second_smallest, second_largest = find_second_element(numbers)
+print("Second smallest:", second_smallest)        
+print("Second largest:", second_largest)
