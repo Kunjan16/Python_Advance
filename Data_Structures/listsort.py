e9b6009f152c5df985373c164a4e489e7243a7e9
Kunjan Kumar Singh <kunjansingh4547@gmail.com>
2026-01-17 00:16:28 +0530

    feat: add functionality to find the largest element in a list

--- Data_Structures/listsort.py
+++ Data_Structures/listsort.py
@@ -16,3 +16,14 @@ for i in range(n):
         if my_list[j] < my_list[least]:
             least = j
     my_list[i], my_list[least] = my_list[least], my_list[i]
+
+
+#largest element in list
+arr = [2,3,5,1,0]
+n = len(arr)
+largest = arr[0]
+for i in range(1, n):
+    if arr[i] > largest:
+        largest = arr[i]
+
+print(largest)            
